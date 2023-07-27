c310636008393e4d168ee90d002cd259626928b5
Andrew Fairclough <andrew@tilden.biol.berkeley.edu>
2023-07-27 00:03:22 -0700

    third commit

--- scripts.py
+++ scripts.py
@@ -20,24 +20,24 @@ except getopt.error as err:
 SampleData = sys.argv[1]
 for read in os.listdir(SampleData):
     SampleName = read.split(".fastq.gz")
-    print(SampleName)
-
-fastqc = "fastqc " + SampleData + "/" + read + " -o " + "/space/s2/andrew/scripts/script_output"
-os.system(fastqc)
-
-cutadapt_output_file = SampleName + list("_ca.fastq.gz")
-cutadapt = "cutadapt -a AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT -o " + cutadapt_output_file + SampleData
-
-fastq_qualitytrimmer_output = SampleName + "fqto.fastq"
-fastq_quality_trimmer = "/home/lenore//bin/fastq_quality_trimmer -v" + " -t " + "20" + " -l " + "20" + " -i " + " -z " + cutadapt_output_file + " -o " + fastq_qualitytrimmer_output
-
-fastqc_quality_trimmer = "fastqc /space/s2/andrew/scripts/" + fastq_qualitytrimmer_output  + " -o " + "/space/s2/andrew/scripts/script_output"
+    FullName = str(SampleName).replace("[","").replace("]","").replace(",","").replace(" ","")
+    print(*SampleName)
+    fastqc = "fastqc " + SampleData + "/" + read + " -o " + "/space/s2/andrew/Output_Dir"
+    os.system(fastqc)
+    cutadapt_output_file = FullName + "_cao.fastq"
+    cutadapt = "cutadapt -a AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT" + " -o " +  "/space/s2/andrew/Output_Dir/" + cutadapt_output_file + " " + SampleData + "/" + read
+    os.system(cutadapt)
+    fastq_qualitytrimmer_output = FullName + "_fqto.fastq"
+    fastq_quality_trimmer = "/home/lenore/bin/fastq_quality_trimmer -v" + " -t " + "20" + " -l " + "20"  + " -i " + cutadapt_output_file + " -o " + "/space/s2/andrew/Output_Dir/" + fastq_qualitytrimmer_output
+    os.system(fastq_quality_trimmer)
+    fastqc_quality_trimmer = "fastqc /space/s2/andrew/Output_Dir/" + fastq_qualitytrimmer_output  + " -o " + "/space/s2/andrew/Output_Dir/CleanedFastq"
+    os.system(fastqc_quality_trimmer)
 
 runSTAR = "/space/s2/andrew/software/STAR-2.7.10b/bin/Linux_x86_64_static/STAR"
 REF_SEQ = "/space/s2/andrew/yeast_genome"
 
-FILES = "ls -m /space/s2/andrew/scripts/fastq_qualitytrimmer_output/*fastq | sed 's/ //g'"
+FILES = "/space/s2/andrew/Output_Dir/CleanedFastq/*_fqto_fastqc.zip"
 FILE = " echo " + FILES + " | sed 's/ //g'"
-runSTARprocess = runSTAR + " --genomeDir " + REF_SEQ + "/STARindex/ --readFilesIn " + FILES + " --outFileNamePrefix aligned_sample --outFilterMultimapNmax 1 --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --twopassMode Basic --runThreadN 1"
+runSTARprocess = runSTAR + " --genomeDir " + REF_SEQ + "/STARindex/ --readFilesIn " + FILES + " --readFilesCommand zcat --outFileNamePrefix /space/s2/andrew/Output_Dir/CleanedFastq/alignedreads/aligned_read --outFilterMultimapNmax 1 --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --twopassMode Basic --runThreadN 1"
 os.system(runSTARprocess)
 
