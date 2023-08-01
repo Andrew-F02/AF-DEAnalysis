8a988f943cabfdbde6a5e13a363d75e9332ad7a5
Andrew Fairclough <andrew@tilden.biol.berkeley.edu>
2023-07-31 21:15:00 -0700

    Added variable for STAR program

--- scripts.py
+++ scripts.py
@@ -17,7 +17,9 @@ try:
 except getopt.error as err:
     print(str(err))
 
+
 SampleData = sys.argv[1]
+Output_Dir = "/space/s2/andrew/Output_Dir/"
 for read in os.listdir(SampleData):
     SampleName = read.split(".fastq.gz")
     FullName = str(SampleName).replace("[","").replace("]","").replace(",","").replace(" ","")
@@ -25,18 +27,17 @@ for read in os.listdir(SampleData):
     fastqc = "fastqc " + SampleData + "/" + read + " -o " + "/space/s2/andrew/Output_Dir"
     os.system(fastqc)
     cutadapt_output_file = FullName + "_cao.fastq"
-    cutadapt = "cutadapt -a AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT" + " -o " +  "/space/s2/andrew/Output_Dir/" + cutadapt_output_file + " " + SampleData + "/" + read
+    cutadapt = "cutadapt -a AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT" + " -o " +  Output_Dir + cutadapt_output_file + " " + SampleData + "/" + read
     os.system(cutadapt)
     fastq_qualitytrimmer_output = FullName + "_fqto.fastq"
-    fastq_quality_trimmer = "/home/lenore/bin/fastq_quality_trimmer -v" + " -t " + "20" + " -l " + "20"  + " -i " + cutadapt_output_file + " -o " + "/space/s2/andrew/Output_Dir/" + fastq_qualitytrimmer_output
+    fastq_quality_trimmer = "/home/lenore/bin/fastq_quality_trimmer -v" + " -t " + "20" + " -l " + "20"  + " -i " + Output_Dir +  cutadapt_output_file + " -o " + Output_Dir + fastq_qualitytrimmer_output
     os.system(fastq_quality_trimmer)
-    fastqc_quality_trimmer = "fastqc /space/s2/andrew/Output_Dir/" + fastq_qualitytrimmer_output  + " -o " + "/space/s2/andrew/Output_Dir/CleanedFastq"
+    fastqc_quality_trimmer = "fastqc " + Output_Dir  + fastq_qualitytrimmer_output  + " -o " + "/space/s2/andrew/Output_Dir/CleanedFastq"
     os.system(fastqc_quality_trimmer)
-   
-Cleaned_Files = FullName + "_fqto_fastqc.zip"
- runSTAR = "/space/s2/andrew/software/STAR-2.7.10b/bin/Linux_x86_64_static/STAR"
- REF_SEQ = "/space/s2/andrew/yeast_genome"
- FILES = "/space/s2/andrew/Output_Dir/CleanedFastq/" + Cleaned_Files
- runSTARprocess = runSTAR + " --genomeDir " + REF_SEQ + "/STARindex/ --readFilesIn " + FILES + " --readFilesCommand zcat --outFileNamePrefix /space/s2/andrew/Output_Dir/CleanedFastq/alignedreads/aligned_read --outFilterMultimapNmax 1 --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --twopassMode Basic --runThreadN 1"
-    os.system(runSTARprocess)
+else:
+     runSTAR = "/space/s2/andrew/software/STAR-2.7.10b/bin/Linux_x86_64_static/STAR"
+     REF_SEQ = "/space/s2/andrew/yeast_genome"
+     Output = Output_Dir + "ERR458815_fqto.fastq," + Output_Dir + "ERR458816_fqto.fastq," + Output_Dir + "ERR458817_fqto.fastq," + Output_Dir + "ERR458818_fqto.fastq" 
+     runSTARprocess = runSTAR + " --genomeDir " + REF_SEQ + "/STARindex/ --readFilesIn " + Output + " --outFileNamePrefix /space/s2/andrew/Output_Dir/CleanedFastq/alignedreads/aligned_read --outFilterMultimapNmax 1 --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --twopassMode Basic --runThreadN 1"
+     os.system(runSTARprocess)
 
