88b768270dcc02d7f6a71888557313a616355bf5
Andrew Fairclough <andrew@tilden.biol.berkeley.edu>
2023-08-03 18:02:24 -0700

    Removed Hardcoded Fastq Trimmer Files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Hello World
+This is a script that takes an input directory of raw RNA sequence data files, in the form of FASTA or FASTQ, and aligns them to an annotated genome. 
--- scripts.py
+++ scripts.py
@@ -20,6 +20,7 @@ except getopt.error as err:
 
 SampleData = sys.argv[1]
 Output_Dir = "/space/s2/andrew/Output_Dir/"
+Cleaned_Files = " "
 for read in os.listdir(SampleData):
     SampleName = read.split(".fastq.gz")
     FullName = str(SampleName).replace("[","").replace("]","").replace(",","").replace(" ","")
@@ -30,14 +31,15 @@ for read in os.listdir(SampleData):
     cutadapt = "cutadapt -a AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT" + " -o " +  Output_Dir + cutadapt_output_file + " " + SampleData + "/" + read
     os.system(cutadapt)
     fastq_qualitytrimmer_output = FullName + "_fqto.fastq"
+    Cleaned_Files = Cleaned_Files + Output_Dir + fastq_qualitytrimmer_output + ","
     fastq_quality_trimmer = "/home/lenore/bin/fastq_quality_trimmer -v" + " -t " + "20" + " -l " + "20"  + " -i " + Output_Dir +  cutadapt_output_file + " -o " + Output_Dir + fastq_qualitytrimmer_output
     os.system(fastq_quality_trimmer)
     fastqc_quality_trimmer = "fastqc " + Output_Dir  + fastq_qualitytrimmer_output  + " -o " + "/space/s2/andrew/Output_Dir/CleanedFastq"
     os.system(fastqc_quality_trimmer)
-else:
-     runSTAR = "/space/s2/andrew/software/STAR-2.7.10b/bin/Linux_x86_64_static/STAR"
-     REF_SEQ = "/space/s2/andrew/yeast_genome"
-     Output = Output_Dir + "ERR458815_fqto.fastq," + Output_Dir + "ERR458816_fqto.fastq," + Output_Dir + "ERR458817_fqto.fastq," + Output_Dir + "ERR458818_fqto.fastq" 
-     runSTARprocess = runSTAR + " --genomeDir " + REF_SEQ + "/STARindex/ --readFilesIn " + Output + " --outFileNamePrefix /space/s2/andrew/Output_Dir/CleanedFastq/alignedreads/aligned_read --outFilterMultimapNmax 1 --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --twopassMode Basic --runThreadN 1"
-     os.system(runSTARprocess)
+
+
+runSTAR= "/space/s2/andrew/software/STAR-2.7.10b/bin/Linux_x86_64_static/STAR"
+REF_SEQ = "/space/s2/andrew/yeast_genome"
+runSTARprocess = runSTAR + " --genomeDir " + REF_SEQ + "/STARindex/ --readFilesIn " + Cleaned_Files + " --outFileNamePrefix /space/s2/andrew/Output_Dir/CleanedFastq/alignedreads/aligned_read --outFilterMultimapNmax 1 --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --twopassMode Basic --runThreadN 1"
+os.system(runSTARprocess)
 
